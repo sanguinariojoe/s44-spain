b2d12d52de1a95163389ad5b5d1db182e2f4cbd9
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-04-04 09:41:31 +0200

    Tweaks to the tanks

--- republicans/units/esr/tanks/ESRRenaultFt17.lua
+++ republicans/units/esr/tanks/ESRRenaultFt17.lua
@@ -1,8 +1,8 @@
 local ESRRenaultFT17 = LightTank:New{
     name                = "Renault FT-17",
     objectName          = "ESR/esrrft17.dae",
-    buildCostMetal      = 2150,
-    maxDamage           = 2130,
+    buildCostMetal      = 850,
+    maxDamage           = 500,
     trackOffset         = 5,
     trackWidth          = 15,
 
@@ -27,6 +27,12 @@ local ESRRenaultFT17 = LightTank:New{
         maxvelocitykmh       = 10,
         barrelrecoildist     = 2,
         normaltex            = "unittextures/ESRRFT17_normals.png",
+        wiki_comments        = [[Renault FT-17 had its time of glory during WWI,
+but it was clearly outdated by 1936. Obviously, the 37mm gun and the low speed
+would make these tanks a great target for the enemy artillery. However, you may
+buy a number of units to other units by a ridiculous preacher.
+
+Indeed, this can be a good close infantry support tank]],
     },
 }
 
--- republicans/units/esr/tanks/ESRT26.lua
+++ republicans/units/esr/tanks/ESRT26.lua
@@ -1,19 +1,19 @@
 local ESRT26 = LightTank:New{
     name                = "T-26",
     objectName          = "ESR/esrt26.dae",
-    buildCostMetal      = 2150,
-    maxDamage           = 2130,
+    buildCostMetal      = 1250,
+    maxDamage           = 750,
     trackOffset         = 5,
     trackWidth          = 15,
 
     weapons = {
         [1] = {
-            name                = "M1938_20K45mmAP",
+            name                = "M1932_20K45mmAP",
             mainDir             = [[0 16 1]],
             maxAngleDif         = 210,
         },
         [2] = {
-            name                = "M1938_20K45mmHE",
+            name                = "M1932_20K45mmHE",
             mainDir             = [[0 16 1]],
             maxAngleDif         = 210,
         },
@@ -27,14 +27,21 @@ local ESRT26 = LightTank:New{
         },
     },
     customParams = {
-        armor_front          = 22,
-        armor_rear           = 8,
-        armor_side           = 14,
+        armor_front          = 15,
+        armor_rear           = 15,
+        armor_side           = 15,
         armor_top            = 10,
         maxammo              = 12,
         maxvelocitykmh       = 24,
         barrelrecoildist     = 3,
         normaltex            = "unittextures/ESRT26_normals.png",
+        wiki_comments        = [[Its 45mm gun and 90hp engine makes this tank
+the very best one of the conflict. T-26 has been designed as a fast tank, able
+to run at 25km/h. However, to achieve such speed the frontal armour had to be
+reduced, up to the 15mm, making it excessively vulnerable AT guns.
+
+Single T-26 tanks would be, in fact, an easy target. But a number of them can
+easily overrun even the strongest enemy line.]],
     },
 }
 
